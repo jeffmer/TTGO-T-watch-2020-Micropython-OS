a9a7dafe7f7d8a720ad233a8cabf604cea883c5c
thiswillbeyourgithub <github@32mail.33mail.com>
2023-07-06 18:44:02 +0200

    fix: don't upload apps related to GPS if V3

--- src/install.sh
+++ src/install.sh
@@ -1,32 +1,50 @@
+#!/usr/bin/env bash
+#set -euxo pipefail
 
 # install .mpy and .png files on target
 
 echo "Making directories" 
 for i in apps drivers utils fonts images clocks
 do
-mpremote u0 fs mkdir $i
+    mpremote a0 fs mkdir $i
 done
 
-mpremote u0 fs mkdir images/weather
+mpremote a0 fs mkdir images/weather
 
 echo "Loading mpy files" 
 for i in ./*.mpy  clocks/*.mpy apps/*.mpy drivers/*.mpy utils/*.mpy fonts/*.mpy
 do
-mpremote u0 fs cp $i :$i
+    # For V3, don't copy the apps that need GPS
+    if [[ $(grep -Rl 'VERSION = 3' config.py) == "config.py" ]]
+    then
+        if [[ $i != "apps/position.mpy" ]] && [[ $i != "apps/maps.mpy" ]] && [[ $i != "./micropyGPS.mpy" ]] && [[ $i != "apps/openstreetmap.mpy" ]]
+        then
+            echo "COPYING $i"
+            mpremote a0 fs cp $i :$i
+        else
+            echo "WILL NOT COPY $i"
+        fi
+    else
+        mpremote a0 fs cp $i :$i
+    fi
 done
 
 echo "Loading image files" 
 for i in images/*.png images/weather/*.png
 do
-mpremote u0 fs cp $i :$i
+    mpremote a0 fs cp $i :$i
 done
 
-echo "Write Boot file" 
-mpremote u0 fs rm :boot.mpy
-mpremote u0 fs cp boot.py :boot.py
+echo "Write Location file"
+mpremote a0 fs cp location.json :location.json
 
-echo "Write Location file" 
-mpremote u0 fs cp location.json :location.json
+echo "Write Boot file"
+mpremote a0 fs rm :boot.mpy
+mpremote a0 fs rm :boot.py
+echo "deleted"
+mpremote a0 fs cp boot.mpy :boot.mpy
+mpremote a0 fs cp boot.py :boot.py
+echo "copied"
 
 echo "Reset"
-mpremote u0 soft-reset
+mpremote a0 soft-reset
